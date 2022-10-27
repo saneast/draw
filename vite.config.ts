ac55badf561116dc954e9b7dfc4fadeeb31ab3c3
saneast <wd0326@foxmail.com>
2022-10-27 23:22:53 +0800

    server host

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  server: {
+    host: '0.0.0.0'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
